ca9c423d30b488096c909aac8bbeb9217e048349
kaneki881 <you@example.com>
2023-11-08 13:21:53 -0800

    fix prob

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Module adds all arguments to a Python list, and then save them to a file"""
-
-
+"""Add all arguments to list and save"""
 import sys
 
 if __name__ == "__main__":
